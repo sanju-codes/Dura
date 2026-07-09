07544b2f0ba7bd7e6daca56f4c40d57832481019
SANJU PAPPACHAN <sanjupappachan8943@gmail.com>
2026-07-09 16:03:04 +0530

    Register service worker on page load
    Added service worker registration to enhance offline capabilities.

--- js/script.js
+++ js/script.js
@@ -668,3 +668,28 @@ console.log(
 
 );
 
+/*==================================================
+REGISTER SERVICE WORKER
+==================================================*/
+
+if("serviceWorker" in navigator){
+
+window.addEventListener("load",()=>{
+
+navigator.serviceWorker.register("service-worker.js")
+
+.then(reg=>{
+
+console.log("Service Worker Registered");
+
+})
+
+.catch(err=>{
+
+console.log(err);
+
+});
+
+});
+
+}
